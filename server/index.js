9b33896ea94b46e3fcc186a3c5c62cc26f34b5ef
Michael Decuire <michaeldecuire18@gmail.com>
2024-10-12 10:57:18 -0500

    Fixed backend routing and project structure.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import cors from "cors";
+import dotenv from 'dotenv';
+import bookRoutes from "./routes/books.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,6 +13,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 const Librarybook = new mongoose.Schema({
     title: String,
@@ -24,11 +29,14 @@ const Librarybook = new mongoose.Schema({
 });
 
 const Book = mongoose.model('book', Librarybook, 'Books');
+export default Book;
 
 // Routing calls
 app.get('/', async (req, res) => {
     res.send("Hello world! ");
 });
+
+app.use('/books', bookRoutes);
 
 // Display all books as a JSON to the user
 // app.get('/Library', async (req, res) => {
